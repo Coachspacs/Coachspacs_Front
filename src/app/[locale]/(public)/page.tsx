21a8d7a49a409f040f85c966e4723cf9fbf73909
hadeelbaroud <baroudhadeel9@gmail.com>
2026-08-08 15:14:41 +0300

    feat: add NewsletterSection component to home page with i18n support

--- src/app/[locale]/(public)/page.tsx
+++ src/app/[locale]/(public)/page.tsx
@@ -8,6 +8,7 @@ import { WhyCoachSpaceStandsOutSection } from "@/components/home/WhyCoachSpaceSt
 import { RealStoriesSection } from "@/components/home/RealStoriesSection";
 import { FaqSection } from "@/components/home/FaqSection";
 import { JoinFutureSection } from "@/components/home/JoinFutureSection";
+import { NewsletterSection } from "@/components/home/NewsletterSection";
 
 export default function HomePage() {
   return (
@@ -17,6 +18,7 @@ export default function HomePage() {
       <MasterYourCraftSection />
       <WhyCoachSpaceStandsOutSection />
       <RealStoriesSection />
+      <NewsletterSection />
       <FaqSection />
       <JoinFutureSection />
     </div>
